8d4c564b5bf7428fe84ad656e5c3482cbc9c274e
pavani <pavaniavni456@gmail.com>
2022-02-09 14:43:46 +0530

    added arraylistexample

---
+++ ArrayList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+namespace ArrayLists
+{
+    public class ArrayList1
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            int t = Convert.ToInt32(Console.ReadLine()) ;
+            for (int i = 0; i < t; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                list.Add(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            foreach (Person p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+
+            public override string ToString()
+            {
+                return Name + " - " + Age;
+            }
+        }
+    }
+}
